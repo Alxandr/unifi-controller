5ecf370d705ee38a6b78361c3bcfba25eb7c2499
Aleksander Heintz <alxandr@alxandr.me>
2018-09-15 15:26:31 +0200

    More debugging

--- travis-install.sh
+++ travis-install.sh
@@ -6,12 +6,13 @@ QEMU_VERSION="v2.9.1-1"
 BUILD_ARCHS="x86_64 aarch64 arm"
 
 ABS_FROM="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ABS_DEST="$ABS_FROM"
 
 sudo docker run --rm --privileged multiarch/qemu-user-static:register
 
-mkdir -p ${ABS_DEST}/qemu ${ABS_FROM}/qemu
+mkdir -p ${ABS_FROM}/qemu
 for target_arch in ${BUILD_ARCHS}; do
 	[[ -f ${ABS_FROM}/qemu/x86_64_qemu-${target_arch}-static.tar.gz ]] || wget -N -P ${ABS_FROM}/qemu https://github.com/multiarch/qemu-user-static/releases/download/${QEMU_VERSION}/x86_64_qemu-${target_arch}-static.tar.gz
-	tar -xvf ${ABS_FROM}/qemu/x86_64_qemu-${target_arch}-static.tar.gz -C ${ABS_DEST}
+	tar -xvf ${ABS_FROM}/qemu/x86_64_qemu-${target_arch}-static.tar.gz -C ${ABS_FROM}/qemu
 done
+
+ls -la ${ABS_FROM}/qemu
